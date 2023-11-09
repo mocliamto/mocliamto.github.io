e5269998afc5a8045de4a368e028dae0075eb2c7
mocliamto <j_l.z@hotmail.com>
2023-11-09 16:25:52 +0100

    test chartjs grow graph

--- src/chartjs/grow.js
+++ src/chartjs/grow.js
@@ -1,164 +1,56 @@
-import Chart from 'chart.js/auto'
+// Data voor de groeigrafiek
+const years = [1, 2, 3, 4]; // Leeftijden in jaren
+const tnoValues = [75, 85, 95, 105]; // Referentiewaarden van TNO
+const userValues = [80, 90, 92, 100]; // Gebruikerswaarden (kan worden vervangen door echte gegevens)
 
-const actions = [
-    {
-        name: 'Stacked: true',
-        handler: (chart) => {
-            chart.options.scales.y.stacked = true;
-            chart.update();
-        }
-    },
-    {
-        name: 'Stacked: false (default)',
-        handler: (chart) => {
-            chart.options.scales.y.stacked = false;
-            chart.update();
-        }
-    },
-    {
-        name: 'Stacked Single',
-        handler: (chart) => {
-            chart.options.scales.y.stacked = 'single';
-            chart.update();
-        }
-    },
-    {
-        name: 'Randomize',
-        handler(chart) {
-            chart.data.datasets.forEach(dataset => {
-                dataset.data = Utils.numbers({count: chart.data.labels.length, min: -100, max: 100});
-            });
-            chart.update();
-        }
-    },
-    {
-        name: 'Add Dataset',
-        handler(chart) {
-            const data = chart.data;
-            const dsColor = Utils.namedColor(chart.data.datasets.length);
-            const newDataset = {
-                label: 'Dataset ' + (data.datasets.length + 1),
-                backgroundColor: dsColor,
-                borderColor: dsColor,
-                fill: true,
-                data: Utils.numbers({count: data.labels.length, min: -100, max: 100}),
-            };
-            chart.data.datasets.push(newDataset);
-            chart.update();
-        }
-    },
-    {
-        name: 'Add Data',
-        handler(chart) {
-            const data = chart.data;
-            if (data.datasets.length > 0) {
-                data.labels = Utils.months({count: data.labels.length + 1});
-
-                for (let index = 0; index < data.datasets.length; ++index) {
-                    data.datasets[index].data.push(Utils.rand(-100, 100));
-                }
-
-                chart.update();
-            }
-        }
-    },
-    {
-        name: 'Remove Dataset',
-        handler(chart) {
-            chart.data.datasets.pop();
-            chart.update();
-        }
-    },
-    {
-        name: 'Remove Data',
-        handler(chart) {
-            chart.data.labels.splice(-1, 1); // remove the label first
-
-            chart.data.datasets.forEach(dataset => {
-                dataset.data.pop();
-            });
-
-            chart.update();
-        }
-    }
-];
-const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
-
-const labels = Utils.months({count: 7});
-const data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'My First dataset',
-            data: Utils.numbers(NUMBER_CFG),
-            borderColor: Utils.CHART_COLORS.red,
-            backgroundColor: Utils.CHART_COLORS.red,
-            fill: true
-        },
-        {
-            label: 'My Second dataset',
-            data: Utils.numbers(NUMBER_CFG),
-            borderColor: Utils.CHART_COLORS.blue,
-            backgroundColor: Utils.CHART_COLORS.blue,
-            fill: true
-        },
-        {
-            label: 'My Third dataset',
-            data: Utils.numbers(NUMBER_CFG),
-            borderColor: Utils.CHART_COLORS.green,
-            backgroundColor: Utils.CHART_COLORS.green,
-            fill: true
-        },
-        {
-            label: 'My Fourth dataset',
-            data: Utils.numbers(NUMBER_CFG),
-            borderColor: Utils.CHART_COLORS.yellow,
-            backgroundColor: Utils.CHART_COLORS.yellow,
-            fill: true
-        }
-    ]
-};
+// Configuratie van de grafiek
+const ctx = document.getElementById('growChartJs').getContext('2d');
 
 const config = {
     type: 'line',
-    data: data,
+    data: {
+        labels: years,
+        datasets: [
+            {
+                label: 'Referentiewaarden TNO',
+                data: tnoValues,
+                borderColor: 'rgba(75, 192, 192, 1)', // Kleur van de lijn
+                backgroundColor: 'rgba(75, 192, 192, 0.2)', // Kleur van het gebied onder de lijn
+                fill: true, // Vul het gebied onder de lijn in
+            },
+            {
+                label: 'Gebruikerswaarden',
+                data: userValues,
+                borderColor: 'rgba(255, 99, 132, 1)', // Kleur van de lijn
+                backgroundColor: 'rgba(255, 99, 132, 0.2)', // Kleur van het gebied onder de lijn
+                fill: true, // Vul het gebied onder de lijn in
+            },
+        ],
+    },
     options: {
         responsive: true,
         plugins: {
             title: {
                 display: true,
-                text: (ctx) => 'Chart.js Line Chart - stacked=' + ctx.chart.options.scales.y.stacked
+                text: 'Groeigrafiek 1-4 jaar',
             },
-            tooltip: {
-                mode: 'index'
-            },
-        },
-        interaction: {
-            mode: 'nearest',
-            axis: 'x',
-            intersect: false
         },
         scales: {
             x: {
                 title: {
                     display: true,
-                    text: 'Month'
-                }
+                    text: 'Leeftijd (jaar)',
+                },
             },
             y: {
-                stacked: true,
                 title: {
                     display: true,
-                    text: 'Value'
-                }
-            }
-        }
-    }
+                    text: 'Lengte (cm)',
+                },
+            },
+        },
+    },
 };
-const ctx = document.getElementById('growChartJs');
 
-new Chart(ctx, {
-    actions: actions,
-    config: config
-});
+// Maak de grafiek
+new Chart(ctx, config);
