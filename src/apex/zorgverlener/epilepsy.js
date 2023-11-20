3f8bd1ef1cb4a1fe9fa9c52921a6eddd95f412f3
mocliamto <j_l.z@hotmail.com>
2023-11-20 14:28:33 +0100

    apexchart epilepsy zorgverlener using mock data

--- src/apex/zorgverlener/epilepsy.js
+++ src/apex/zorgverlener/epilepsy.js
@@ -1,110 +1,131 @@
-var chart = new ApexCharts(document.querySelector("#epi-chart-line"), options);
-chart.render();
+fetch('../../assets/epilepsy.json')
+    .then(response => response.json())
+    .then(data => {
+        const sortedAanvalsregistratie = data.Epilepsie_tijdslijn.Aanvalsregistratie.sort((a, b) => new Date(a.datum) - new Date(b.datum));
+        const sortedMedicatie = data.Medicatie.sort((a, b) => new Date(a.datum) - new Date(b.datum));
+        const sortedPDD_DDD = data.PDD_DDD.sort((a, b) => new Date(a.datum) - new Date(b.datum));
+        const sortedLabuitslagen_en_ketonen = data.Labuitslagen_en_ketonen.sort((a, b) => new Date(a.datum) - new Date(b.datum));
 
-var optionsLine2 = {
-    series: [{
-        data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-            min: 10,
-            max: 30
-        })
-    }],
-    chart: {
-        id: 'tw',
-        group: 'social',
-        type: 'line',
-        height: 300
-    },
-    title: {
-        text: 'Aanvalsregistratie',
-        align: 'left'
-    },
-    colors: ['#546E7A'],
-    stroke: {
-        curve: 'smooth',
-        width: 3
-    }
-};
+        var optionsLine2 = {
+            series: [{
+                name: 'Aanvalsregistratie',
+                data: sortedAanvalsregistratie.map(item => ({
+                    x: new Date(item.datum),
+                    y: item.aantal
+                }))
+            }],
+            chart: {
+                id: 'tw',
+                group: 'social',
+                type: 'line',
+                height: 300
+            },
+            title: {
+                text: 'Aanvalsregistratie',
+                align: 'left'
+            },
+            colors: ['#546E7A'],
+            stroke: {
+                width: 3
+            },
+            xaxis: {
+                type: 'datetime'
+            }
+        };
 
-var chartLine2 = new ApexCharts(document.querySelector("#epi-chart-line2"), optionsLine2);
-chartLine2.render();
+        var chartLine2 = new ApexCharts(document.querySelector("#epi-chart-line2"), optionsLine2);
+        chartLine2.render();
 
-var optionsArea = {
-    series: [{
-        data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-            min: 10,
-            max: 60
-        })
-    }],
-    chart: {
-        id: 'yt',
-        group: 'social',
-        type: 'area',
-        height: 300
-    },
-    title: {
-        text: 'Medicatie',
-        align: 'left'
-    },
-    colors: ['#00E396'],
-    stroke: {
-        curve: 'smooth',
-        width: 3
-    }
-};
+        var optionsArea = {
+            series: [{
+                name: 'Medicatie',
+                data: sortedMedicatie.map(item => ({
+                    x: new Date(item.datum),
+                    y: item.dosis
+                }))
+            }],
+            chart: {
+                id: 'yt',
+                group: 'social',
+                type: 'line',
+                height: 300
+            },
+            title: {
+                text: 'Medicatie',
+                align: 'left'
+            },
+            colors: ['#00E396'],
+            stroke: {
+                width: 3
+            },
+            xaxis: {
+                type: 'datetime'
+            }
+        };
 
-var chartArea = new ApexCharts(document.querySelector("#epi-chart-area"), optionsArea);
-chartArea.render();
+        var chartArea = new ApexCharts(document.querySelector("#epi-chart-area"), optionsArea);
+        chartArea.render();
 
-var optionsSmall = {
-    series: [{
-        data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-            min: 10,
-            max: 60
-        })
-    }],
-    chart: {
-        id: 'ig',
-        group: 'social',
-        type: 'area',
-        height: 300
-    },
-    title: {
-        text: 'PDD/DDD',
-        align: 'left'
-    },
-    colors: ['#008FFB'],
-    stroke: {
-        curve: 'smooth',
-        width: 3
-    }
-};
+        var optionsSmall = {
+            series: [{
+                name: 'PDD/DDD',
+                data: sortedPDD_DDD.map(item => ({
+                    x: new Date(item.datum),
+                    y: item.PDD_DDD_waarde
+                }))
+            }],
+            chart: {
+                id: 'yt',
+                group: 'social',
+                type: 'line',
+                height: 300
+            },
+            title: {
+                text: 'PDD/DDD',
+                align: 'left'
+            },
+            colors: ['#008FFB'],
+            stroke: {
+                width: 3
+            },
+            xaxis: {
+                type: 'datetime'
+            }
+        };
 
-var chartSmall = new ApexCharts(document.querySelector("#epi-chart-small"), optionsSmall);
-chartSmall.render();
+        var chartSmall = new ApexCharts(document.querySelector("#epi-chart-small"), optionsSmall);
+        chartSmall.render();
 
-var optionsSmall2 = {
-    series: [{
-        data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-            min: 10,
-            max: 30
-        })
-    }],
-    chart: {
-        id: 'li',
-        group: 'social',
-        type: 'area',
-        height: 300
-    },
-    title: {
-        text: 'Labuitslagen',
-        align: 'left'
-    },
-    colors: ['#546E7A'],
-    stroke: {
-        curve: 'smooth',
-        width: 3
-    }
-};
+        var optionsSmall2 = {
+            series: [{
+                name: 'Labuitslagen en ketonen',
+                data: sortedLabuitslagen_en_ketonen.map(item => ({
+                    x: new Date(item.datum),
+                    y: item.ketonen_mmol_l
+                }))
+            }],
+            chart: {
+                id: 'li',
+                group: 'social',
+                type: 'line',
+                height: 300
+            },
+            title: {
+                text: 'Labuitslagen en ketonen',
+                align: 'left'
+            },
+            colors: ['#546E7A'],
+            stroke: {
+                width: 3
+            },
+            xaxis: {
+                type: 'datetime'
+            }
+        };
 
-var chartSmall2 = new ApexCharts(document.querySelector("#epi-chart-small2"), optionsSmall2);
-chartSmall2.render();
+        var chartSmall2 = new ApexCharts(document.querySelector("#epi-chart-small2"), optionsSmall2);
+        chartSmall2.render();
+    })
+    .catch(error => {
+        console.error("Error fetching the data: ", error);
+    });
